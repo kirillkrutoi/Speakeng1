1c4cb399e1f00cb51dbe607da2648ccee5379533
kirillkrutoi <157674482+kirillkrutoi@users.noreply.github.com>
2024-01-25 09:56:13 +0400

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,19 @@
+<?php
+
+/*$host = localhost;
+$db = u2389906_SPEAKENG;
+$user = u2389906_SPEAKEN;
+$password = u2389906_SPEAKEN;
+$table = users;
+
+
+$connection = new mysqli($host, $db, $user, $password, $table);
+
+if ($connection -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $connection -> connect_error;
+  exit();
+}*/
+$EMAIL = $_POST["EMAIL"];
+echo $EMAIL;
+
+?>
